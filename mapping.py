7a48c68cff6cd7ebbbd1638b963f1d7468b55c27
IDRIS ABDELLAH <idrisabdellah@outlook.fr>
2024-05-31 16:43:51 +0200

    fix

--- mapping.py
+++ mapping.py
@@ -1,9 +1,6 @@
-CONTROLLER_ROBOT = {"00:E9:3A:68:C1:04": "DC:A6:32:BF:B2:82",
-                    "D4:1B:81:3A:E1:AA": "DC:A6:32:BF:B2:4F",
-                    "00:E9:3A:68:2F:0E": "DC:A6:32:BF:B2:19",
-                    "00:E9:3A:68:2F:1C": "DC:A6:32:BF:B1:98",
+CONTROLLER_ROBOT = {"00:E9:3A:68:C0:E8": "DC:A6:32:BF:B2:82",
+
                     "00:E9:3A:68:2F:26": "DC:A6:32:BF:B0:FF",
-                    "00:E9:3A:68:C0:B4": "DC:A6:32:BF:91:CD"
                     }
 
 # Inverting keys and values
